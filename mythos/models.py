d26bdb6543bb721b624a849056360aeb77c681c9
Sergio <ses110@gmail.com>
2014-03-17 18:23:45 -0500

    Models: Heroes, Gods, Events

--- mythos/models.py
+++ mythos/models.py
@@ -3,31 +3,31 @@ from django.db import models
 class Relations(models.Model):
    pass 
 
-class Figure(Models.Model):
+class Hero(Models.Model):
     '''
-    Figure Model
+    Hero Model
 
-    @type figure_id: models.CharField(200, primary_key = True)
-    @cvar figure_id: A figure's primary key to uniquely the instance
+    @type hero_id: models.CharField(200, primary_key = True)
+    @cvar hero_id: A hero's primary key to uniquely the instance
     
     '''
-    figure_id = models.CharField(max_length=200, primary_key=True)
-    name      = models.CharField(max_length=200)
+    hero_id = models.CharField(max_length=200, primary_key=True)
+    name    = models.CharField(max_length=200)
     def getID(self):
         """
         Fetch primary key ID from current instance
 
-        @type self: Figure
-        @param self: Current Figure instance
+        @type self: Hero
+        @param self: Current Hero instance
 
         @rtype: string
-        @return: The id associated with this Figure instance  
+        @return: The id associated with this Hero instance  
         """
-        return self.figure_id
+        return self.hero_id
 
 class Event(Models.Model):
     event_id = models.CharField(max_length=200, primary_key=True)
-    name      = models.CharField(max_length=200)
+    name     = models.CharField(max_length=200)
     def getID(self):
         """
         Fetch primary key ID from current instance
@@ -40,18 +40,18 @@ class Event(Models.Model):
         """
         return self.event_id
 
-class Faction(Models.Model):
-    faction_id = models.CharField(max_length=200, primary_key=True)
-    name      = models.CharField(max_length=200)
+class God(Models.Model):
+    god_id = models.CharField(max_length=200, primary_key=True)
+    name   = models.CharField(max_length=200)
 
     def getID(self):
         """
         Fetch primary key ID from current instance
 
-        @type self: Faction
-        @param self: Current Faction instance
+        @type self: God
+        @param self: Current God instance
 
         @rtype: string
-        @return: The id associated with this Faction instance
+        @return: The id associated with this God instance
         """
-        return self.faction_id
+        return self.god_id
