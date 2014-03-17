95cb6497b2d83d92ab2b3ac10f906c2a2cefc8d7
comyarzaheri <comyarzaheri@utexas.edu>
2014-03-17 18:15:17 -0500

    modified url pattern

--- mythos/urls.py
+++ mythos/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from mythos import views
 
 urlpatterns = patterns('',
-        url(r'^mythos/', include('mythos.urls')))
+        url(r'', include('mythos.urls')))
