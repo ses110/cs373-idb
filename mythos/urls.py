a199cf65e2724a51d71c6e93e034155add51e26e
comyarzaheri <comyarzaheri@utexas.edu>
2014-03-17 18:17:56 -0500

    modified url pattern for index page

--- mythos/urls.py
+++ mythos/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-from mythos import views
 
 urlpatterns = patterns('',
-        url(r'', include('mythos.urls')))
+        url(r'^$', 'mythos.views.index'))
