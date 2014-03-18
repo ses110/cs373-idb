50b218c32b6dbe4aff2e10d7ca66f282fc18b9c7
comyarzaheri <comyarzaheri@utexas.edu>
2014-03-18 17:28:19 -0500

    started defining list templates

--- mythos/views.py
+++ mythos/views.py
@@ -87,8 +87,15 @@ def stories(request):
     context = RequestContext(request)
 
     # Get list of all figures
+    stories = [    
+        models.Story(name='The Rape of Persephone'),
+        models.Story(name='Hercules and his Labours'),
+        models.Story(name='Perseus and Medusa'),
+        models.Story(name='Labyrinth of Thebes'),
+        models.Story(name='Titans and Zeus')
+    ]
 
     # TESTING
-    context_dict = {'title':'Stories'}
+    context_dict = {'title':'Stories', 'items':stories}
 
     return render_to_response('mythos/stories.html', context_dict, context)
