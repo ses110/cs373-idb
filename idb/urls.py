7d8c84ec4456f9d7be77afa1dcac922041d23560
comyarzaheri <comyarzaheri@utexas.edu>
2014-03-18 13:24:37 -0500

    added templates for cultures, stories and modified urls.py to add url paths

--- idb/urls.py
+++ idb/urls.py
@@ -8,5 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^figures/$', views.figures, name='figures'),
-        url(r'^figures/(\w+)/$', views.figure, name='figure')
+        url(r'^figures/(\w+)/$', views.figure, name='figure'),
+        url(r'^cultures/$', views.cultures, name='cultures'),
+        url(r'^cultures/(\w+)/$', views.culture, name='culture'),
+        url(r'^stories/$', views.stories, name='stories'),
+        url(r'^stories/(\w+)/$', views.story, name='story')
 )
