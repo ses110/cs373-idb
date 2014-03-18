34ae921031481a3b8ee521e13d42aa56a7e47931
comyarzaheri <comyarzaheri@utexas.edu>
2014-03-17 20:01:04 -0500

    modified urls.py for index

--- idb/urls.py
+++ idb/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^mythos/', include('mythos.urls')),
+    url(r'^$', include('mythos.urls')),
 )
--- mythos/views.py
+++ mythos/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render_to_response
 # Create your views here.
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': '#yoloswag'}
+    context_dict = {'boldmessage': '#yoloswag', 'test': 'YOLOOOOO'}
     return render_to_response('mythos/index.html', context_dict, context)
