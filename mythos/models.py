dd1fd3eca53bf48333dce5c7a7e38aa5eef2b0d6
comyarzaheri <comyarzaheri@utexas.edu>
2014-03-20 21:03:58 -0500

    updated models for many to many assocations and regenerated docs

--- mythos/models.py
+++ mythos/models.py
@@ -51,20 +51,20 @@ class Figure(models.Model):
     @cvar kind: Kind of the figure
     @type biography: models.CharField(10000)
     @cvar biography: Short description/bio of the figure
-    @type related_figures: models.ForeignKey(self)
-    @cvar related_figures: Foreign key associating other related figures to this figure instance
-    @type related_cultures: models.ForeignKey('Culture')
-    @cvar related_cultures: Foreign key associating related cultures to this figure instance
-    @type related_stories: models.ForeignKey('Story')
-    @cvar related_stories: Foreign key associating related stories that this figure instance is in
+    @type related_figures: models.ManyToManyField('self')
+    @cvar related_figures: Many to many assocation associating other related figures to this figure instance
+    @type related_cultures: models.ManyToManyField('Culture')
+    @cvar related_cultures: Many to many assocation associating related cultures to this figure instance
+    @type related_stories: models.ManyToManyField('Story')
+    @cvar related_stories: Many to many assocation associating related stories that this figure instance is in
     '''
     id                  = models.IntegerField(primary_key=True)
     name                = models.CharField(max_length=200)
     kind                = models.CharField(max_length=200)
     biography           = models.CharField(max_length=10000)
-    related_figures     = models.ForeignKey('self')
-    related_cultures    = models.ForeignKey('Culture')
-    related_stories     = models.ForeignKey('Story')
+    related_figures     = models.ManyToManyField('self')
+    related_cultures    = models.ManyToManyField('Culture')
+    related_stories     = models.ManyToManyField('Story')
 
     def getID(self):
         """
@@ -88,19 +88,19 @@ class Story(models.Model):
     @cvar name: Name of Story instance
     @type summary: models.CharField(10000)
     @cvar summary: Short description/summary of the story
-    @type related_figures: models.ForeignKey('Figure')
-    @cvar related_figures: Foreign Key linking to any related figures associated with this story
-    @type related_cultures: models.ForeignKey('Culture')
-    @cvar related_cultures: Foreign Key linking to any related cultures that may be associated with this story
-    @type related_stories: models.ForeignKey('Story')
-    @cvar related_stories: Foreign key linking to any other stories that may be related to this story instance
+    @type related_figures: models.ManyToManyField('Figure')
+    @cvar related_figures: Many to many assocation linking to any related figures associated with this story
+    @type related_cultures: models.ManyToManyField('Culture')
+    @cvar related_cultures: Many to many assocation linking to any related cultures that may be associated with this story
+    @type related_stories: models.ManyToManyField('self')
+    @cvar related_stories: Many to many assocation linking to any other stories that may be related to this story instance
     '''
     id                  = models.IntegerField(primary_key=True)
     name                = models.CharField(max_length=200)
     summary             = models.CharField(max_length=10000)
-    related_figures     = models.ForeignKey('Figure')
-    related_cultures    = models.ForeignKey('Culture')
-    related_stories     = models.ForeignKey('self')
+    related_figures     = models.ManyToManyField('Figure')
+    related_cultures    = models.ManyToManyField('Culture')
+    related_stories     = models.ManyToManyField('self')
 
     def getID(self):
         """
@@ -128,21 +128,21 @@ class Culture(models.Model):
     @cvar history: Summary of the history of this Culture instance
     @type language: models.CharField(200)
     @cvar language: Most common language spoken in this culture
-    @type related_figures: models.ForeignKey('Figure')
-    @cvar related_figures: Foreign Key associating Figures related to this Culture
-    @type related_cultures: models.ForeignKey('self')
-    @cvar related_cultures: Foreign key associating Cultures similar to this Culture
-    @type related_stories: models.ForeignKey('Story')
-    @cvar related_stories:  Foreign Key associating notable mythological stories belonging within this Culture
+    @type related_figures: models.ManyToManyField('Figure')
+    @cvar related_figures: Many to many assocation associating Figures related to this Culture
+    @type related_cultures: models.ManyToManyField('self')
+    @cvar related_cultures: Many to many assocation associating Cultures similar to this Culture
+    @type related_stories: models.ManyToManyField('Story')
+    @cvar related_stories:  Many to many assocation associating notable mythological stories belonging within this Culture
     '''
     id                  = models.IntegerField(primary_key=True)
     name                = models.CharField(max_length=200)
     region              = models.CharField(max_length=200)
     history             = models.CharField(max_length=10000)
     language            = models.CharField(max_length=200)
-    related_figures     = models.ForeignKey('Figure')
-    related_cultures    = models.ForeignKey('self')
-    related_stories     = models.ForeignKey('Story')
+    related_figures     = models.ManyToManyField('Figure')
+    related_cultures    = models.ManyToManyField('self')
+    related_stories     = models.ManyToManyField('Story')
 
     def getID(self):
         """
